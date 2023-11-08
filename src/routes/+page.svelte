c670189d3e1fe1e5796c06182006808b573e290b
Kevin Cao <kjcao@proton.me>
2023-11-08 15:27:56 -0500

    Add title, TOC & ability to reset
    Add title and description meta tags. Add ability to reset viewer. Add
    rudimentary table of contents.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import ePub from 'epubjs';
 
 	let dragover = false;
+	let panel, metadata, blank = true;
 
 	function getFile(event) {
 		let file;
@@ -20,24 +21,42 @@
 
 	function tryRender() {
 		localForage.getItem('ebook', (err, value) => {
-			if (err) { return; }
+			if (err) { alert('fuck'); return; }
+			if (!value) {
+				blank = true;
+				return;
+			}
+			blank = false;
 			let book = ePub(value);
 			let rendition = book.renderTo(
 				document.body,
 				{
 					manager: "continuous",
 					flow: "scrolled", width: "100%"
-					});
+				});
 			let displayed = rendition.display();
 
-			// displayed.then(renderer => { });
-			// book.loaded.navigation.then(toc => {
-			// 	console.log(toc);
-			// });
+			book.loaded.metadata.then(x => { metadata = x; });
+			book.loaded.navigation.then(toc => {
+				toc.forEach((chapter, index) => {
+					const x = document.querySelector('.toc');
+					
+					let item = document.createElement('li');
+					let link = document.createElement('a');
+					link.id = `chapter-${chapter.id}`;
+					link.href = chapter.href;
+					link.innerText = chapter.label.trim();
+					link.classList.add('link');
+					link.onclick = () => {
+						rendition.display(link.getAttribute("href"));
+						return false;
+					};
+					item.appendChild(link);
+					x.appendChild(item);
+				});
+			});
 		})
 	}
-
-	let panel;
 
 	onMount(() => {
 		tryRender();
@@ -46,7 +65,6 @@
 			e.preventDefault();
 			e.dataTransfer.dropEffect = 'copy';
 			panel.style.visibility = 'visible';
-
 		};
 		const hide = e => {
 			if (!e.fromElement) {
@@ -81,19 +99,53 @@
 			document.removeEventListener('dragleave', hide);
 		}
 	});
-
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>{metadata?.title ?? "untitled"}</title>
+	<meta name="description" content={metadata?.description ?? "No description."} />
 </svelte:head>
 
 <main>
+	{#if blank}
+		<div class="center">
+			<h1>Drop file to get started!</h1>
+		</div>
+	{:else}
+		<div class="left">
+			<div>
+				<a on:click={() => {
+					localForage.clear();
+					location.reload();
+					return false;
+				}}>restart</a>
+			</div>
+			<ul class="toc"></ul>
+		</div>
+	{/if}
 	<div bind:this={panel} class="dropZone"></div>
 </main>
 
 <style>
+	.center {
+		text-align: center;
+	}
+
+	.toc {
+		list-style: none;
+		padding: 0;
+	}
+
+	.left {
+		position: fixed;
+		top: 0;
+		left: 0;
+		border: 2px solid black;
+		border-radius: 16px;
+		padding: 1em 2em;
+		margin: 1em 1em;
+	}
+
 	.dropZone {
 		background: gray;
 		position: fixed;
@@ -104,5 +156,26 @@
 		z-index: 999;
 		opacity: 0.6;
 		visibility: hidden;
+	}
+
+	.left a {
+		text-decoration: none;
+		color: inherit;
+		font-family: monospace;
+		text-transform: uppercase;
+	}
+
+	:global(.epub-container) {
+		background: white;
+	}
+
+	:global(.left a) {
+		text-decoration: none;
+		color: inherit;
+		font-family: monospace;
+	}
+
+	:global(.left a:hover) {
+		font-weight: 700;
 	}
 </style>
